986ab294161b4915172fb859ff1a5b8bd287c688
mifontelera1 <mifontelera1@gmail.com>
2020-01-30 06:06:41 +0800

    Updates

---
+++ precache-manifest.14976ed961027286bf0a0dd9aa951520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d618a8845b2f66959b4b28a5eb5b88a2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a9eb238f72c5ed370aa3",
+    "url": "/my-app/static/css/main.2e6c3fb5.chunk.css"
+  },
+  {
+    "revision": "c586403b1f342a4a93ed",
+    "url": "/my-app/static/js/2.16dedccb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.16dedccb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9eb238f72c5ed370aa3",
+    "url": "/my-app/static/js/main.2b370c2d.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
